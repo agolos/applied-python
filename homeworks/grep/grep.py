f9ab9048fdd3bc6abb47cb11f2aa228be1121bdd
Alexandr Golosseyev <mates@inbox.ru>
2020-01-26 11:24:13 +0300

    Created solutuion for grep
    Created fix

--- homeworks/grep/grep.py
+++ homeworks/grep/grep.py
@@ -1,5 +1,5 @@
-
 import argparse
+import re
 import sys
 
 
@@ -8,10 +8,51 @@ def output(line):
 
 
 def grep(lines, params):
-    for line in lines:
-        line = line.rstrip()
-        if params.pattern in line:
-            output(line)
+    counter = 0
+    processed_lines_list = []
+    res_list = []
+    # line_number block
+    if params.line_number:
+        lines = [str(i + 1) + ':' + lines[i] for i in range(len(lines))]
+    # context block
+    if params.context:
+        params.after_context = params.before_context = params.context
+    # regular block
+    params.pattern = params.pattern.replace('?', '\w')
+    params.pattern = params.pattern.replace('*', '\w*')
+    # ignore case block
+    params.pattern = params.pattern.upper() if params.ignore_case else params.pattern
+    for i in range(len(lines)):
+        line_tmp = lines[i].rstrip().upper() if params.ignore_case else lines[i].rstrip()
+        # invert value block
+        if params.invert:
+            if re.search(params.pattern, line_tmp) is None and i not in processed_lines_list:
+                processed_lines_list.append(i)
+                res_list.append(lines[i])
+        elif re.search(params.pattern, line_tmp) is not None:
+            if params.count:
+                counter += 1
+            elif params.before_context or params.after_context:
+                start_ind, stop_ind = i - params.before_context, i + 1 + params.after_context
+                for j in range(start_ind, stop_ind):
+                    if (j % len(lines)) not in processed_lines_list:
+                        processed_lines_list.append(j)
+                        if params.line_number:
+                            res_list.append(
+                                lines[j % len(lines)] if i == j else lines[j % len(lines)].replace(':', '-'))
+                        else:
+                            res_list.append(lines[j % len(lines)])
+                    elif (j % len(lines)) == i and params.line_number:
+                        ''' code for strings that were defined in after or before blocks with line number param, 
+                        but match with pattern'''
+                        res_list[res_list.index(lines[i].replace(':', '-'))] = lines[i].replace('-', ':')
+            elif i not in processed_lines_list:
+                processed_lines_list.append(i)
+                res_list.append(lines[i])
+    for line in res_list:
+        output(line)
+    if counter:
+        output(str(counter))
 
 
 def parse_args(args):
